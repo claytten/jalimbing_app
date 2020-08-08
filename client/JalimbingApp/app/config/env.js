0d4cd398d34af5a424f7982214f1c13e80705932
ramafebri <rama_febri48@yahoo.com>
2020-08-08 11:39:41 +0700

    delete

--- client/JalimbingApp/app/config/env.js
+++
@@ -1,4 +0,0 @@
-export const YOUTUBE_API = 'AIzaSyCsF9jKjcZLnv3umJWn-VcSO_h312jjMF8';
-export const MAPS_API = 'AIzaSyB4dMYw0-tlVWfJ6zN-u7tqLCwI-6k4xic';
-export const API_URL = 'http://127.0.0.1:8000/api/';
-export const IMAGE_URL = 'http://127.0.0.1:8000/storage/';
