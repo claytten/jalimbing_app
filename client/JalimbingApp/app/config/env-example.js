3c6fda27db399e4d9851ce21646e08ca48d77eae
ramafebri <rama_febri48@yahoo.com>
2020-08-08 11:45:40 +0700

    update env

--- client/JalimbingApp/app/config/env-example.js
+++ client/JalimbingApp/app/config/env-example.js
@@ -1,2 +1,4 @@
-const YOUTUBE_API = 'YOUR API';
-export default YOUTUBE_API;
+export const YOUTUBE_API = 'YOUTUBE_API';
+export const MAPS_API = 'MAPS_API';
+export const API_URL = 'http://127.0.0.1:8000/api/';
+export const IMAGE_URL = 'http://127.0.0.1:8000/storage/';
