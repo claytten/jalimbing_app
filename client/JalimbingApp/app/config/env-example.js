b6bf911030d150f5956bba5fb8f0c2f0fd20f334
ramafebri <rama_febri48@yahoo.com>
2020-08-13 08:16:11 +0700

    update

--- client/JalimbingApp/app/config/env-example.js
+++ client/JalimbingApp/app/config/env-example.js
@@ -1,4 +1,4 @@
 export const YOUTUBE_API = 'YOUR_YOUTUBE_API';
 export const MAPS_API = 'YOUR_MAPS_API';
-export const API_URL = 'http://mobile.senijalimbing.com/api/';
-export const IMAGE_URL = 'http://mobile.senijalimbing.com/storage/';
+export const API_URL = 'https://mobile.senijalimbing.com/api/';
+export const IMAGE_URL = 'https://mobile.senijalimbing.com/storage/';
